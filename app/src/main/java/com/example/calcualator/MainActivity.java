7c5ffb3171cd79291c942293e196379729b91da3
lsao <lahcenenligne@gmail.com>
2023-09-25 16:16:52 +0100

    building all widget and xml element

--- app/src/main/java/com/example/calcualator/MainActivity.java
+++ app/src/main/java/com/example/calcualator/MainActivity.java
@@ -2,13 +2,153 @@ package com.example.calcualator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
-
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
 public class MainActivity extends AppCompatActivity {
+    Button one, two, three, four, five, sex, seven, eight, nine, zero, division, multiplication, substract, addition, rightkaws, leftkaws, oss, point, clear, equals;
 
+    EditText inputText;
+    String textValue;
+    int EqualValue, firstNum, secondNum;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        one            = (Button) findViewById(R.id.one);
+        two            = (Button) findViewById(R.id.two);
+        three          = (Button) findViewById(R.id.three);
+        four           = (Button) findViewById(R.id.four);
+        five           = (Button) findViewById(R.id.five);
+        sex            = (Button) findViewById(R.id.sex);
+        seven          = (Button) findViewById(R.id.seven);
+        eight          = (Button) findViewById(R.id.eight);
+        nine           = (Button) findViewById(R.id.nine);
+        zero           = (Button) findViewById(R.id.zero);
+        division       = (Button) findViewById(R.id.division);
+        substract      = (Button) findViewById(R.id.substract);
+        addition       = (Button) findViewById(R.id.addition);
+        multiplication = (Button) findViewById(R.id.multiplication);
+        oss            = (Button) findViewById(R.id.oss);
+        point          = (Button) findViewById(R.id.point);
+        rightkaws      = (Button) findViewById(R.id.kawsright);
+        leftkaws       = (Button) findViewById(R.id.kawsleft);
+        clear          = (Button) findViewById(R.id.clear);
+        equals         = (Button) findViewById(R.id.equals);
+        inputText      = (EditText) findViewById(R.id.textValue);
+        textValue      = inputText.getText().toString();
+        /**
+         * get all the value of the button and convert it into integer number
+         */
+        final int ONE   = Integer.parseInt(one.getText().toString());
+        final int TWO   = Integer.parseInt(two.getText().toString());
+        final int THREE = Integer.parseInt(three.getText().toString());
+        final int FOUR  = Integer.parseInt(four.getText().toString());
+        final int FIVE  = Integer.parseInt(five.getText().toString());
+        final int SEX   = Integer.parseInt(sex.getText().toString());
+        final int SEVEN = Integer.parseInt(seven.getText().toString());
+        final int EIGHT = Integer.parseInt(eight.getText().toString());
+        final int NINE  = Integer.parseInt(nine.getText().toString());
+        final int ZERO  = Integer.parseInt(zero.getText().toString());
+        one.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                inputText.setText(textValue += String.valueOf(ONE));
+            }
+        });
+        two.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                inputText.setText(textValue += String.valueOf(TWO));}
+        });
+        three.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                inputText.setText(textValue += String.valueOf(THREE));
+            }
+        });
+        four.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                inputText.setText(textValue += String.valueOf(FOUR));
+            }
+        });
+        five.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                inputText.setText(textValue += String.valueOf(FIVE));
+            }
+        });
+        sex.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                inputText.setText(textValue += String.valueOf(SEX));
+            }
+        });
+        seven.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                inputText.setText(textValue += String.valueOf(SEVEN));
+            }
+        });
+        eight.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                inputText.setText(textValue += String.valueOf(EIGHT));
+            }
+        });
+        nine.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(textValue.equalsIgnoreCase("")){
+                    inputText.setText(String.valueOf(NINE));
+                }else{
+                    inputText.setText(textValue += String.valueOf(NINE));
+                }
+            }
+        });
+        zero.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                inputText.setText(textValue += String.valueOf(ZERO));
+            }
+        });
+        clear.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                inputText.setText("");
+                textValue = "";
+                firstNum = 0;
+                secondNum = 0;
+                EqualValue = 0;
+            }
+        });
+        addition.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(textValue.equalsIgnoreCase("") || textValue.isEmpty()){
+
+                }else{
+                    firstNum += Integer.parseInt(textValue);
+                    inputText.setText("");
+                    textValue = "";
+                }
+            }
+        });
+        equals.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(textValue.equalsIgnoreCase("")){
+
+                }else{
+                    secondNum = Integer.parseInt(textValue);
+                    EqualValue = firstNum + secondNum;
+                    inputText.setText(String.valueOf(EqualValue));
+                }
+            }
+        });
     }
+
 }
